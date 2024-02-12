15f95c1e825ad97dea0a56987586b5ccd4b547af
tentikevinn <153823653+tentikevinn@users.noreply.github.com>
2024-02-12 15:38:14 +0100

    Update TesterMagazzino.java

--- GestioneMagazzino/src/gestione/prodotti/TesterMagazzino.java
+++ GestioneMagazzino/src/gestione/prodotti/TesterMagazzino.java
@@ -1,10 +1,14 @@
 package gestione.prodotti;
-import java.util.*;     
-
 public class TesterMagazzino {
 	public static void main(String[] args) throws Exception {
-		Magazzino a = new Magazzino(2, 2);
-		Prodotto b = new Prodotto("Marca", "Modello", 10, 50);
-		Prodotto c = new Prodotto("Apple", "15", 10, 50);
+	
+		Magazzino a=new Magazzino(1,1);
+		
+		Prodotto b=new Prodotto("Marca", "Modello", 10, 50);
+		Prodotto c=new Prodotto("Marca", "Modello", 10, 50);
+
+		a.aggiungiProdotto(b);
+		a.aggiungiProdotto(c);
+		a.stampa();
 	}
 }
